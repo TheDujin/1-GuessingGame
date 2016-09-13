a9783919b975dfcf207c776dd62c19aabed5fa56
U-BSD\293359 <293359@SHS-10D8A831B.beaverton.k12.or.us>
2016-09-13 14:17:11 -0700

    Finished project

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
-int main() {
-  int input = 0;
-  cout << input << endl;
-  cin >> input;
-  cout << input;
+  int main() {
+  srand(time(NULL));
+  bool isPlaying = true;
+  int guess = 0;
+  int numGuesses = 0;
+  char yesNo = 'a';
+  while (isPlaying == true) {
+    numGuesses = 0;
+    int ans = rand() % 100 + 1;
+    cout << "Welcome to Guessing Game!" << endl << "Please input a guess." << endl;
+    cin >> guess;
+    numGuesses++;
+    while (guess != ans) {
+      if (guess > ans)
+	cout << "Too high!" << endl;
+      else
+	cout << "Too low!" << endl;
+      cin >> guess;
+      numGuesses++;
+    }
+    cout << "Congrats, you win!" << endl << "You used " << numGuesses << " guesses." << endl << "Play again? (y, n)" << endl;
+    cin >> yesNo;
+    if (yesNo != 'y'){
+      isPlaying = false;
+      cout << "Thanks for playing!";
+    }
+}
   return 0;
 }
