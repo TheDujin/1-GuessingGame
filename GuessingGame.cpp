ae47d33cbe3d48036f38c47d7a5b49c68074b0c2
U-BSD\293359 <293359@SHS-10D8A831B.beaverton.k12.or.us>
2016-09-15 13:33:10 -0700

    Added comments

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -2,21 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+//Guessing Game by Kevin Jin
+//A game about guessing numbers
 using namespace std;
-
+//Main method which runs everything
   int main() {
   srand(time(NULL));
   bool isPlaying = true;
   int guess = 0;
   int numGuesses = 0;
   char yesNo = 'a';
+  //While the user still wants to play...
   while (isPlaying == true) {
     numGuesses = 0;
+    //Generates a new number
     int ans = rand() % 100 + 1;
     cout << "Welcome to Guessing Game!" << endl << "Please input a guess." << endl;
     cin >> guess;
     numGuesses++;
+    //While the answer is wrong...
     while (guess != ans) {
       if (guess > ans)
 	cout << "Too high!" << endl;
@@ -27,6 +31,7 @@ using namespace std;
     }
     cout << "Congrats, you win!" << endl << "You used " << numGuesses << " guesses." << endl << "Play again? (y, n)" << endl;
     cin >> yesNo;
+    //If character read in isn't y, end the game
     if (yesNo != 'y'){
       isPlaying = false;
       cout << "Thanks for playing!";
